7217eec377b049e7ca99382ef1a998d75ce97340
paisaningk <gaw.fd@hotmail.com>
2021-08-30 18:12:32 +0700

    add CameraController

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Script
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Func<Vector3> getCameraPoitionFunc;
+        [SerializeField] private Transform Player;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            getCameraPoitionFunc = () => Player.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var cameraFollwPoition = getCameraPoitionFunc();
+            cameraFollwPoition.z = transform.position.z;
+
+            Vector3 cameraMoveDir = (cameraFollwPoition - transform.position).normalized;
+            float disance = Vector3.Distance(cameraFollwPoition,transform.position);
+            float camraMoveSpped = 1;
+
+            if (disance > 0 )
+            {
+                Vector3 newCameraPointon = transform.position + cameraMoveDir * disance * camraMoveSpped * Time.deltaTime;
+
+                float distanceAfterMoving = Vector3.Distance(newCameraPointon,cameraFollwPoition);
+
+                if (distanceAfterMoving > disance)
+                {
+                    //Overshot the target 
+                    newCameraPointon = cameraFollwPoition;
+                }
+
+                transform.position = newCameraPointon;
+            }
+
+            
+        }
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,79 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Assets.Script
 {
-    [SerializeField] private LayerMask dashLayerMask;
-    private Rigidbody2D Rd;
-    private Vector3 MoveDie;
-    private const float MoveSpeed = 5f;
-    private Animator animator;
-    private bool isDashButtonDown;
-
-    private void Awake()
+    public class Player : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-        Rd = GetComponent<Rigidbody2D>();
-    }
+        [SerializeField] private LayerMask dashLayerMask;
+        private Rigidbody2D Rd;
+        private Vector3 MoveDie;
+        private const float MoveSpeed = 5f;
+        private Animator animator;
+        private bool isDashButtonDown;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            Rd = GetComponent<Rigidbody2D>();
+        }
     
 
-    // Update is called once per frame
-    private void Update()
-    {
-        float MoveX = 0f;
-        float MoveY = 0f;
+        // Update is called once per frame
+        private void Update()
+        {
+            float MoveX = 0f;
+            float MoveY = 0f;
         
-        Rd.MoveRotation(0);
+            Rd.MoveRotation(0);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            MoveY = +1f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            MoveY = -1f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            MoveX = -1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            MoveX = +1f;
-        }
-        
-        MoveDie = new Vector3(MoveX,MoveY).normalized;
-        
-        animator.SetFloat("MoveX",MoveX);
-        animator.SetFloat("MoveY",MoveY);
-        animator.SetFloat("Speed",MoveDie.sqrMagnitude);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            isDashButtonDown = true;
-        }
-        
-    }
-
-    private void FixedUpdate()
-    {
-        Rd.velocity = MoveDie * MoveSpeed;
-
-        if (isDashButtonDown == true)
-        {
-            float dashAmount = 3f;
-
-            Vector3 dashPoint = transform.position + MoveDie * dashAmount;
-
-            RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, MoveDie, dashAmount,dashLayerMask);
-            if (raycastHit2D.collider != null)
+            if (Input.GetKey(KeyCode.W))
             {
-                dashPoint = raycastHit2D.point;
+                MoveY = +1f;
             }
+            if (Input.GetKey(KeyCode.S))
+            {
+                MoveY = -1f;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                MoveX = -1f;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                MoveX = +1f;
+            }
+        
+            MoveDie = new Vector3(MoveX,MoveY).normalized;
+        
+            animator.SetFloat("MoveX",MoveX);
+            animator.SetFloat("MoveY",MoveY);
+            animator.SetFloat("Speed",MoveDie.sqrMagnitude);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isDashButtonDown = true;
+            }
+        
+        }
+
+        private void FixedUpdate()
+        {
+            Rd.velocity = MoveDie * MoveSpeed;
+
+            if (isDashButtonDown == true)
+            {
+                float dashAmount = 3f;
+
+                Vector3 dashPoint = transform.position + MoveDie * dashAmount;
+
+                RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, MoveDie, dashAmount,dashLayerMask);
+                if (raycastHit2D.collider != null)
+                {
+                    dashPoint = raycastHit2D.point;
+                }
             
-            Rd.MovePosition(dashPoint);
-            isDashButtonDown = false;
+                Rd.MovePosition(dashPoint);
+                isDashButtonDown = false;
+            }
         }
     }
 }
