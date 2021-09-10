796043575df5a591b0d3be88fc240020cefa2740
paisaningk <gaw.fd@hotmail.com>
2021-09-11 02:37:10 +0700

    add CharacterBase aand PlayerCharacter

---
+++ Assets/Script/Base/CharacterBase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Script.Base
+{
+    public abstract class CharacterBase : MonoBehaviour
+    {
+        protected string Name { get; set; }
+        public int Hp { get; protected set; }
+        public int Atk { get; protected set; }
+        public float MovementSpeed { get; private set; }
+
+        protected void SetCharact(string name,int hp,int atk,float movementSpeed)
+        {
+            Name = name;
+            Hp = hp;
+            Atk = atk;
+            MovementSpeed = movementSpeed;
+        }
+    
+        public abstract void OnTriggerEnter2D(Collider2D other);
+    
+    }
+}
