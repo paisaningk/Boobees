faf15c9c18587b5468a4dece5c5e1eaa188f29b5
paisaningk <gaw.fd@hotmail.com>
2021-09-11 04:45:16 +0700

    Add PlayerCharacter

--- Assets/Script/Base/PlayerCharacter.cs
+++ Assets/Script/Base/PlayerCharacter.cs
@@ -1,8 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Assets.Script.scriptableobject;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour
+namespace Assets.Script.Base
 {
+    public class PlayerCharacter : MonoBehaviour
+    {
+        [SerializeField] private CharacterSo Player;
     
+        private string name;
+        private int hp;
+        private int Atk;
+        private float Speed;
+    
+        public void Start()
+        {
+            name = Player.Name;
+            hp = Player.MaxHp;
+            Atk = Player.Atk;
+            Speed = Player.Speed;
+            PrintAll();
+        }
+
+        public void PrintAll()
+        {
+            Debug.Log($"name:{name}");
+            Debug.Log($"HP:{hp}");
+            Debug.Log($"ATK:{Atk}");
+            Debug.Log($"Speed:{Speed}");
+        }
+    }
 }
