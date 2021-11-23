4eb790ec79ba4e45728787e1d76ab3714f820360
paisaningk <gaw.fd@hotmail.com>
2021-11-23 23:24:02 +0700

    all fix

--- Assets/Script/Test01.cs
+++ Assets/Script/Test01.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,26 @@ using UnityEngine;
 public class Test01 : MonoBehaviour
 {
     // Start is called before the first frame update
+    private float time = 5f;
     void Start()
     {
-        StartCoroutine(Shoping());
+        //adc();
     }
-    
-    
-    IEnumerator Shoping()
+
+    private void Update()
     {
-        Debug.Log("eeee");
-        yield return new WaitForSeconds(3);
-        Debug.Log("adc");
+        if (time >= 0)
+        {
+            adc();
+        }
+        
     }
+
+    private void adc()
+    {
+        var a = (time -= Time.deltaTime) / 60;
+        Debug.Log($"deltaTime = {Time.deltaTime}");
+        Debug.Log(a);
+    }
+    
 }
